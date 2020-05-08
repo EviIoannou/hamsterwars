56e14ffed683af23192dd860d6186aec09bd8cd3
unknown <evievi@outlook.com>
2020-05-08 14:03:15 +0200

    Can now get pictures and data for all hamsters

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'))
+app.use('/assets', express.static('pictures'));
 app.use(express.json());
 
 //Require routes
-const assetsRoute = require('./routes/assets');
-app.use('/assets', assetsRoute);
+// const assetsRoute = require('./routes/assets');
+// app.use('/assets', assetsRoute);
 
-const chartsRoute = require('./routes/charts');
-app.use('/charts', chartsRoute);
+// const chartsRoute = require('./routes/charts');
+// app.use('/charts', chartsRoute);
 
-const gamesRoute = require('./routes/games');
-app.use('/games', gamesRoute);
+// const gamesRoute = require('./routes/games');
+// app.use('/games', gamesRoute);
 
 const hamstersRoute = require('./routes/hamsters');
 app.use('/hamsters', hamstersRoute);
 
-const statsRoute = require('./routes/stats');
-app.use('/stats', statsRoute);
+// const statsRoute = require('./routes/stats');
+// app.use('/stats', statsRoute);
 
 //App listening to port 3000
 app.listen(3000, () => {
